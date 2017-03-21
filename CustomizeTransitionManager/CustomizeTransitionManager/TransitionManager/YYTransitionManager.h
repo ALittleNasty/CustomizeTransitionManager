c1e7862327711fcdf3bad40904541e9e155606f7
ALittleNasty <379643671@qq.com>
2017-03-21 16:16:51 +0800

    Add Manager

---
+++ CustomizeTransitionManager/CustomizeTransitionManager/TransitionManager/YYTransitionManager.h
@@ -0,0 +1,13 @@
+//
+//  YYTransitionManager.h
+//  CustomizeTransitionManager
+//
+//  Created by ALittleNasty on 2017/3/21.
+//  Copyright © 2017年 ALittleNasty. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface YYTransitionManager : NSObject
+
+@end
---
+++ CustomizeTransitionManager/CustomizeTransitionManager/TransitionManager/YYTransitionManager.m
@@ -0,0 +1,13 @@
+//
+//  YYTransitionManager.m
+//  CustomizeTransitionManager
+//
+//  Created by ALittleNasty on 2017/3/21.
+//  Copyright © 2017年 ALittleNasty. All rights reserved.
+//
+
+#import "YYTransitionManager.h"
+
+@implementation YYTransitionManager
+
+@end
