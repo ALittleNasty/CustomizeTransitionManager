7a639c5f084277c25c1bb64f19cfceef99a1df89
ALittleNasty <379643671@qq.com>
2017-03-21 16:10:59 +0800

    Initialize
    Initialize project

---
+++ CustomizeTransitionManager/CustomizeTransitionManager/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CustomizeTransitionManager
+//
+//  Created by ALittleNasty on 2017/3/21.
+//  Copyright © 2017年 ALittleNasty. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CustomizeTransitionManager/CustomizeTransitionManager/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CustomizeTransitionManager
+//
+//  Created by ALittleNasty on 2017/3/21.
+//  Copyright © 2017年 ALittleNasty. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
